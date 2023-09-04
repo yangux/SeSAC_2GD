9ea12d3413c8b41c8275ad6462c9683605edc918
yangux <yeonie05@gmail.com>
2023-09-04 16:53:21 +0900

    react: 스타일 적용하기

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,80 +1,48 @@
-// import UseJSX from "./components/UseJSX";
-// import FirstPractice from "./components/practice/FirstPractice";
-// import ClassCom from "./components/ClassCom";
-// import FunctionState from "./components/FunctionState";
-// import ClassState from "./components/ClassState";
-// import PracticeState from "./components/practice/PracticeState";
-// import FunctionProps from "./components/FunctionProps";
-// import PracticeProps from "./components/practice/PracticeProps";
-// import { ClassProps, ClassProps2 } from "./components/practice/ClassProps";
-// import { useState } from "react";
-// import HandlerEx from "./components/practice/HandlerEx";
-// import HandlerEx2 from "./components/practice/HandlerEx2";
-// import HandlerEx3 from "./components/practice/HandlerEx3";
-// import HandlerEx4 from "./components/practice/HandlerEx4";
-
-import PropsMap from "./components/PropsMap";
-import PororoObj from "./components/practice/PororoObj";
-import PracticeMap from "./components/practice/PracticeMap";
+import logo from "./logo.svg";
+import styled, { keyframes } from "styled-components";
 
 function App() {
-  // return (
-  // <div>
-  //   <UseJSX />
-  //   <FirstPractice />
-  //   <ClassCom />
-  //   <FunctionState />
-  //   <ClassState />
-  //   <PracticeState />
-  //   <FunctionProps name="사과" krPrice={5000} number={10} />
-  //   <FunctionProps name="샤인머스캣" krPrice={35000} number={3} />
-  //   <FunctionProps name="복숭아" />
-  //   <FunctionProps>여기가 children입니다.</FunctionProps>
-  //   <PracticeProps />
-  //   <ClassProps name="뽀로로" nickname="사고뭉치" color="#0186cd" />
-  //   <ClassProps2 name="루피" nickname="공주" bgColor="#ed9095" />
-  // );
-  // function test() {
-  //   console.log("test");
-  // }
-  // const handleClick = (e) => {
-  //   // console.log(e);
-  //   e.preventDefault();
-  // };
-  // return (
-  //   <>
-  //     {/* html에서 a태그의 고유 이벤트(하이퍼링크)를 막는 방법: onclick="return false" */}
-  //     <a href="https://naver.com" onClick={handleClick}>
-  //       a 태그입니다.
-  //     </a>
-  //     <button onClick={test}>버튼</button>
-  //   </>
-  // );
-  // return (
-  //   <>
-  //     <HandlerEx />
-  //     <HandlerEx2 />
-  //     <hr />
-  //     <HandlerEx3 />
-  //     <hr />
-  //     <HandlerEx4 />
-  //   </>
-  // );
-
-  const dataArr = [
-    { name: "peach", krPrice: 10000, number: 5 },
-    { name: "strawberry", krPrice: 14000, number: 1 },
-    { name: "pear", krPrice: 5000, number: 6 },
-    { name: "apple", krPrice: 10000, number: 14 },
-  ];
+  const RootDiv = styled.div`
+    text-align: center;
+  `;
+  const AppHeader = styled.header`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+  const AppLogoSpin = keyframes`
+    from{ transform: rotate(0)}
+    to{transform: rotate(360deg)}
+  `;
+  const AppLogo = styled.img`
+    height: 40vmin;
+    pointer-events: none;
+    animation: ${AppLogoSpin} 20s infinite linear;
+  `;
+  const MyA = styled.a`
+    color: #61dafb;
+  `;
   return (
-    <>
-      <PropsMap arr={dataArr} />
-      <PracticeMap />
-      <hr />
-      <PororoObj />
-    </>
+    <RootDiv>
+      <AppHeader>
+        <AppLogo src={logo} alt="app" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <MyA
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </MyA>
+      </AppHeader>
+    </RootDiv>
   );
 }
-
 export default App;
