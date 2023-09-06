ef362aafa94979b07ef67fe81709130b8d360c5d
yangux <yeonie05@gmail.com>
2023-09-06 15:36:05 +0900

    SCSS

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -1,48 +1,11 @@
-import logo from "./logo.svg";
-import styled, { keyframes } from "styled-components";
+import UseScss from "./components/UseScss";
+import "./styles/style.scss";
 
 function App() {
-  const RootDiv = styled.div`
-    text-align: center;
-  `;
-  const AppHeader = styled.header`
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  `;
-  const AppLogoSpin = keyframes`
-    from{ transform: rotate(0)}
-    to{transform: rotate(360deg)}
-  `;
-  const AppLogo = styled.img`
-    height: 40vmin;
-    pointer-events: none;
-    animation: ${AppLogoSpin} 20s infinite linear;
-  `;
-  const MyA = styled.a`
-    color: #61dafb;
-  `;
   return (
-    <RootDiv>
-      <AppHeader>
-        <AppLogo src={logo} alt="app" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MyA
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </MyA>
-      </AppHeader>
-    </RootDiv>
+    <>
+      <UseScss />
+    </>
   );
 }
 export default App;
---
+++ react-start/src/components/UseScss.jsx
@@ -0,0 +1,55 @@
+import img1 from "../assets/img1.png";
+import UseScss01 from "./UseScss01";
+
+export default function UseScss() {
+  return (
+    <>
+      <div className="div1">
+        <div className="div2">
+          <div className="div3"></div>
+        </div>
+        <button className="btn orangered">BUTTON1</button>
+        <button className="btn btn--opacity">BUTTON2</button>
+        <button className="btn btn--blue">BUTTON3</button>
+      </div>
+
+      <div className="container">
+        <div className="box1"></div>
+        <div className="box1"></div>
+        <div className="box1"></div>
+        <p className="circle"></p>
+        <p className="circle"></p>
+        <p className="circle"></p>
+        <div className="box2">HI1</div>
+        <div className="box2">HI2</div>
+        <div className="box2">HI3</div>
+        <div className="box2">HI4</div>
+      </div>
+      <hr />
+      <h2>이미지 적용하기</h2>
+      <h4>1. src 폴더 내부의 이미지 가져오기</h4>
+      <img src="../assets/img1.png" alt="경로명으로 접근" />
+      <img src={img1} alt="import로 접근" />
+      <div className="src-img img-test"></div>
+
+      <h4>2. public 폴더 내부의 이미지 가져오기</h4>
+      <img src="/img1.png" alt="public 폴더 사진 접근" />
+      <img
+        src={process.env.PUBLIC_URL + "/img1.png"}
+        alt="public 폴더 사진 접근 - 조금 더 안전한 방법"
+      />
+      <div className="public-img img-test"></div>
+      <hr />
+
+      <h1>실습문제</h1>
+      <div className="practice">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <UseScss01 />
+    </>
+  );
+}
